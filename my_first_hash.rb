711f91350686f728bd93af50671954408d0e7163
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-01 02:18:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,9 +26,9 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+shipping_manifest["oil paintings"]
 end
 
-shipping_manifest["oil paintings"]
 
 def adding
   shipping_manifest = {
